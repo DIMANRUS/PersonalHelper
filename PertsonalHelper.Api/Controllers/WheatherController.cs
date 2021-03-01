1c5d9269413b5fb1d7a2abc9b33ecd51dacdc150
DIMANRUS <41239551+DIMANRUS@users.noreply.github.com>
2021-03-01 21:28:17 +0300

    Add api

---
+++ PertsonalHelper.Api/Controllers/WheatherController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace PertsonalHelper.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpGet("get")]
+        public int GetWheather()
+        {
+            return 5;
+        }
+    }
+}
