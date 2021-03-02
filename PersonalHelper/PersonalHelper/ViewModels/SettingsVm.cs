a3af0331cd92aa6c15746b43963760640c05f2e2
DIMANRUS <dmitrysorokin2017@outlook.com>
2021-03-02 20:55:09 +0300

    Add Settings VM

---
+++ PersonalHelper/PersonalHelper/ViewModels/SettingsVm.cs
@@ -0,0 +1,10 @@
+﻿using PersonalHelper.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonalHelper.ViewModels {
+    class SettingsVm {
+        public string UserName { get => User.GetUserName(); }
+    }
+}
