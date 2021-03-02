a01be31ffed8a0ccb27ae8f5b25d4f6be56195ee
LeoWarp <leowarp@bk.ru>
2021-03-02 21:15:49 +0300

    Commit

--- PertsonalHelper.Api/Controllers/WeatherController.cs
+++ PertsonalHelper.Api/Controllers/WeatherController.cs
@@ -1,20 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.AspNetCore.Mvc;
+using PersonalHelper.Models;
+using System.IO;
+using System.Net;
+using System.Runtime.Serialization.Json;
+using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace PertsonalHelper.Api.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/[controller]/")]
     public class WeatherController : ControllerBase
     {
         [HttpGet]
-        public int GetWheather()
+        public IActionResult Get()
         {
-            return 5;
+
+            string url = "http://api.openweathermap.org/data/2.5/weather?q=London";
+
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+
+            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+            string response = "";
+
+            using (StreamReader sr = new StreamReader(httpWebResponse.GetResponseStream()))
+            {
+                response = sr.ReadToEnd();
+            }
+
+            return Ok();
         }
     }
 }
--- PertsonalHelper.Api/Startup.cs
+++ PertsonalHelper.Api/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -12,6 +13,14 @@ namespace PertsonalHelper.Api {
         public IConfiguration Configuration { get; }
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
+            services.Configure<RouteOptions>(options =>
+            {
+                options.LowercaseUrls = true;
+                options.AppendTrailingSlash = true;
+            });
+
             services.AddControllers();
         }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
