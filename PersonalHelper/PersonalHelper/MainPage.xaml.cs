9a0d3d74d799c482333469d04b0ee30e690964ce
DIMANRUS <dmitrysorokin2017@outlook.com>
2021-03-04 20:37:30 +0300

    Fix

--- PersonalHelper/PersonalHelper/MainPage.xaml.cs
+++ PersonalHelper/PersonalHelper/MainPage.xaml.cs
@@ -1,20 +1,11 @@
 ﻿using PersonalHelper.ViewModels;
 using Xamarin.Forms;
-<<<<<<< HEAD
-namespace PersonalHelper {
-    public partial class MainPage : ContentPage {
-        public MainPage() =>
-=======
-
 namespace PersonalHelper
 {
     public partial class MainPage : ContentPage
     {
-        MainPageVM mainPageModel;
-
-        public MainPage() 
-        {
->>>>>>> LeoWarp
+        public MainPage() {
             InitializeComponent();
+        }
     }
 }
