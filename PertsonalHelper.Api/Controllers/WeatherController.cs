2eb083e4424bff3314fb3f946f5e4a5c032d7f4a
LeoWarp <leowarp@bk.ru>
2021-03-04 21:23:53 +0300

    v3.1

--- PertsonalHelper.Api/Controllers/WeatherController.cs
+++ PertsonalHelper.Api/Controllers/WeatherController.cs
@@ -1,11 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using PersonalHelper.Api.Models;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace PertsonalHelper.Api.Controllers
