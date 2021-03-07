15215466ea15a6dd331cc801ce3aa1c2081dbcda
DIMANRUS <dmitrysorokin2017@outlook.com>
2021-03-07 16:42:54 +0300

    Fix

---
+++ PersonalHelper/PersonalHelper/Helpers/HttpHelper.cs
@@ -0,0 +1,8 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PersonalHelper.Helpers {
+    class HttpHelper {
+        public async Task<string> HttpRequest(string url) => await (new HttpClient()).GetStringAsync(url);         
+    }
+}
