18cb653f9b6fdd18e8c26290a83efab90d34005a
DIMANRUS <dmitrysorokin2017@outlook.com>
2021-03-09 10:46:31 +0300

    Страница авторизации переделана под MVVM

--- PersonalHelper/PersonalHelper/Views/Auth.xaml.cs
+++ PersonalHelper/PersonalHelper/Views/Auth.xaml.cs
@@ -1,18 +1,14 @@
 ﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Xamarin.Essentials;
-using PersonalHelper.Models;
-using PersonalHelper.Helpers;
 
 namespace PersonalHelper.Views {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Auth : ContentPage
     {
-        RootJsonCities cities;
         public Auth()
         {
             InitializeComponent();
-            cities = FilesHelper.GetRootJsonCities();
         }
         private void RadioButton_CheckedChanged(object sender, CheckedChangedEventArgs e)
         {
@@ -30,24 +26,6 @@ namespace PersonalHelper.Views {
                     Application.Current.UserAppTheme = OSAppTheme.Light;
                     break;
             }
-        }
-        private async void ShowLocationInformation_Clicked(object sender, System.EventArgs e) => await DisplayAlert("Зачем это нужно?", "На основе вашего введённого города будет показываться погода, новости и другая информация", "Ок");
-
-        private async void NavigateToMainPage_Click(object sender, System.EventArgs e)
-        {
-            if (User.VerifyCity(city.Text))
-            {
-                if (name.Text.Length == 0)
-                    await DisplayAlert("Ошибка", "Введите имя", "Ок");
-                else
-                {
-                    User.SetUserName(name.Text);
-                    User.SetUserCity(city.Text);
-                    await Navigation.PushModalAsync(new MainPage(), true);
-                }
-            }
-            else
-                await DisplayAlert("Ошибка", "Город не найден", "Ок");
         }
     }
 }
--- PertsonalHelper.Api/Controllers/WeatherController.cs
+++ PertsonalHelper.Api/Controllers/WeatherController.cs
@@ -11,31 +11,31 @@ namespace PertsonalHelper.Api.Controllers
     [Route("api/[controller]/")]
     public class WeatherController : ControllerBase
     {
-        [HttpGet("{city}")]
-        public async Task<IActionResult> Get(string city)
-        {
-            // сервер получение погоды 
-            //string url = $"http://api.openweathermap.org/data/2.5/weather?q={city}&appid=7cfd697d8dcb30e786d31dd24802a29d&units=metric";
+        //[HttpGet("{city}")]
+        //public async Task<IActionResult> Get(string city)
+        //{
+        //    // сервер получение погоды 
+        //    //string url = $"http://api.openweathermap.org/data/2.5/weather?q={city}&appid=7cfd697d8dcb30e786d31dd24802a29d&units=metric";
 
-            //HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+        //    //HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
 
-            //HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+        //    //HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
 
-            //string response = "";
+        //    //string response = "";
 
-            //using (StreamReader sr = new StreamReader(httpWebResponse.GetResponseStream())) {
-            //    response = await sr.ReadToEndAsync();
-            //}
+        //    //using (StreamReader sr = new StreamReader(httpWebResponse.GetResponseStream())) {
+        //    //    response = await sr.ReadToEndAsync();
+        //    //}
 
-            //RootJsonWheather result = JsonConvert.DeserializeObject<RootJsonWheather>(response);
+        //    //RootJsonWheather result = JsonConvert.DeserializeObject<RootJsonWheather>(response);
 
-            //return Ok(result.weather.First().description + @"/" + result.main.temp);
+        //    //return Ok(result.weather.First().description + @"/" + result.main.temp);
 
 
-            //HttpClient http = new HttpClient();
-            //string json = await http.GetStringAsync($"http://api.openweathermap.org/data/2.5/weather?q={city}&appid=7cfd697d8dcb30e786d31dd24802a29d&units=metric");
-            //RootJsonWheather result = JsonConvert.DeserializeObject<RootJsonWheather>(json);
-            //return Ok(result.weather.First().description + @"/" + result.main.temp);
-        }
+        //    //HttpClient http = new HttpClient();
+        //    //string json = await http.GetStringAsync($"http://api.openweathermap.org/data/2.5/weather?q={city}&appid=7cfd697d8dcb30e786d31dd24802a29d&units=metric");
+        //    //RootJsonWheather result = JsonConvert.DeserializeObject<RootJsonWheather>(json);
+        //    //return Ok(result.weather.First().description + @"/" + result.main.temp);
+        //}
     }
 }
