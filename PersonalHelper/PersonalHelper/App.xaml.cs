87fe981fa9fe28b1611679b0f2778f6a98f14f95
DIMANRUS <dmitrysorokin2017@outlook.com>
2021-03-16 21:23:59 +0300

    Fix Nav Bar Color, Fix NullRefernceExeption in auth

--- PersonalHelper/PersonalHelper/App.xaml.cs
+++ PersonalHelper/PersonalHelper/App.xaml.cs
@@ -6,16 +6,21 @@ namespace PersonalHelper {
     public partial class App : Application {
         public App() {
             InitializeComponent();
-            _ = User.GetUserTheme() switch
-            {
+            _ = User.GetUserTheme() switch {
                 "Dark" => Current.UserAppTheme = OSAppTheme.Dark,
                 "Light" => Current.UserAppTheme = OSAppTheme.Light,
                 _ => Current.UserAppTheme = OSAppTheme.Dark
             };
             if (User.GetUserName() == "null")
-                MainPage = new NavigationPage(new Auth()) { BarBackgroundColor = (User.GetUserTheme() == "Light") ? Color.White : Color.Black };
+                MainPage = new NavigationPage(new Auth()) {
+                    BarBackgroundColor = (User.GetUserTheme() == "Light") ? Color.White : Color.Black,
+                    BarTextColor = (User.GetUserTheme() == "Light") ? Color.Black : Color.White
+                };
             else
-                MainPage = new NavigationPage(new MainPage()) { BarBackgroundColor = (User.GetUserTheme() == "Light") ? Color.White : Color.Black };
+                MainPage = new NavigationPage(new MainPage()) {
+                    BarBackgroundColor = (User.GetUserTheme() == "Light") ? Color.White : Color.Black,
+                    BarTextColor = (User.GetUserTheme() == "Light") ? Color.Black : Color.White
+                };
 
         }
         protected override void OnStart() {
