ca14a52872a89518013e0cd6b0d8d303e29b8f16
DIMANRUS <41239551+DIMANRUS@users.noreply.github.com>
2021-04-04 10:00:23 +0300

    Fix for ios

---
+++ PersonalHelper/PersonalHelper.iOS/NotificationEventArgs.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PersonalHelper.iOS {
+    class NotificationEventArgs : EventArgs {
+        public string Title { get; set; }
+        public string Message { get; set; }
+    }
+}
