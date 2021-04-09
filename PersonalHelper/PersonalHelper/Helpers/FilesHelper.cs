55e19b94e4b6d949257b027929a4e283bc7b76a8
DIMANRUS <41239551+DIMANRUS@users.noreply.github.com>
2021-04-09 19:35:51 +0300

    Fix

--- PersonalHelper/PersonalHelper/Helpers/FilesHelper.cs
+++ PersonalHelper/PersonalHelper/Helpers/FilesHelper.cs
@@ -5,18 +5,25 @@ using System.Threading.Tasks;
 using PersonalHelper.Models;
 using Xamarin.Essentials;
 
-namespace PersonalHelper.Helpers {
-    static class FilesHelper {
+namespace PersonalHelper.Helpers
+{
+    static class FilesHelper
+    {
         private static IEnumerable<Cities> cities;
-        public async static Task<IEnumerable<Cities>> GetRootJsonCities() {
-            if (cities == null) {
-                using (var stream = await FileSystem.OpenAppPackageFileAsync("cities.json")) {
-                    using (var reader = new StreamReader(stream)) {
+        public async static Task<IEnumerable<Cities>> GetRootJsonCities()
+        {
+            if (cities == null)
+            {
+                using (var stream = await FileSystem.OpenAppPackageFileAsync("cities.json"))
+                {
+                    using (var reader = new StreamReader(stream))
+                    {
                         string a = await reader.ReadToEndAsync();
                         return JsonSerializer.Deserialize<RootJsonCities>(a).Cities;
                     }
                 }
-            } else
+            }
+            else
                 return cities;
         }
     }
